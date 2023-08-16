48be60bcddc23f6787a7f9bc59133692fe8212dc
angryOil <jipmj13@gmail.com>
2023-08-16 15:47:23 +0900

    제로벨류(값을 지정안했을경우) + 문자열 템플릿

---
+++ ch2/stringTemplate.go
@@ -0,0 +1,8 @@
+package main
+
+import "fmt"
+
+func StrTemplate() {
+	fmt.Println(`Greetings and "salutations"`)
+	fmt.Println("Greetings \n\"salutations\"")
+}
---
+++ ch2/zeroValue.go
@@ -0,0 +1,9 @@
+package main
+
+import "fmt"
+
+func BoolZero() {
+	var flag bool
+	var isAwesome = true
+	fmt.Println(flag, ":", isAwesome)
+}
